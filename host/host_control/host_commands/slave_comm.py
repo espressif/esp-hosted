e9b2241583a83fd9cece3e359c4b226ab2319d4c
ajita.chavan <ajita.chavan@espressif.com>
2020-06-09 20:35:39 +0530

    Changes in slave_comm.py and host driver Makefile
    Signed-off-by: ajita.chavan <ajita.chavan@espressif.com>

--- host/host_control/host_commands/slave_comm.py
+++ host/host_control/host_commands/slave_comm.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import transport
-import slave_config_pb2
+import esp_hosted_config_pb2
 import binascii
 
 class Aplist:
@@ -39,8 +39,8 @@ failure = "failure"
 # mode == 1 for station mac
 # mode == 2 for softAP mac
 def get_mac(mode):
-    req_sta_mac = slave_config_pb2.SlaveConfigPayload()
-    req_sta_mac.msg = slave_config_pb2.SlaveConfigMsgType.TypeCmdGetMACAddress
+    req_sta_mac = esp_hosted_config_pb2.EspHostedConfigPayload()
+    req_sta_mac.msg = esp_hosted_config_pb2.EspHostedConfigMsgType.TypeCmdGetMACAddress
     #for station mode set cmd as 1
     req_sta_mac.cmd_get_mac_address.cmd = str(mode)
     protodata = req_sta_mac.SerializeToString()
@@ -60,8 +60,8 @@ def get_mac(mode):
 # 3: softAP+station mode
 
 def get_wifi_mode():
-     req_wifi_mode = slave_config_pb2.SlaveConfigPayload()
-     req_wifi_mode.msg = slave_config_pb2.SlaveConfigMsgType.TypeCmdGetWiFiMode
+     req_wifi_mode = esp_hosted_config_pb2.EspHostedConfigPayload()
+     req_wifi_mode.msg = esp_hosted_config_pb2.EspHostedConfigMsgType.TypeCmdGetWiFiMode
      protodata = req_wifi_mode.SerializeToString()
      #print("serialized data "+str(protodata))
      tp = transport.Transport_pserial(interface)
@@ -78,8 +78,8 @@ def get_wifi_mode():
 # 2: softAP mode
 # 3: softAP+station mode
 def set_wifi_mode(mode):
-     set_wifi_mode = slave_config_pb2.SlaveConfigPayload()
-     set_wifi_mode.msg = slave_config_pb2.SlaveConfigMsgType.TypeCmdSetWiFiMode
+     set_wifi_mode = esp_hosted_config_pb2.EspHostedConfigPayload()
+     set_wifi_mode.msg = esp_hosted_config_pb2.EspHostedConfigMsgType.TypeCmdSetWiFiMode
      set_wifi_mode.cmd_set_wifi_mode.mode = mode
      protodata = set_wifi_mode.SerializeToString()
      #print("serialized data "+str(protodata))
@@ -94,8 +94,8 @@ def set_wifi_mode(mode):
 # get AP config to which ESP station is connected
 # It returns ssid, bssid, channel and rssi
 def wifi_get_ap_config():
-     get_ap_config = slave_config_pb2.SlaveConfigPayload()
-     get_ap_config.msg = slave_config_pb2.SlaveConfigMsgType.TypeCmdGetAPConfig
+     get_ap_config = esp_hosted_config_pb2.EspHostedConfigPayload()
+     get_ap_config.msg = esp_hosted_config_pb2.EspHostedConfigMsgType.TypeCmdGetAPConfig
      protodata = get_ap_config.SerializeToString()
      #print("serialized data "+str(protodata))
      tp = transport.Transport_pserial(interface)
@@ -113,8 +113,8 @@ def wifi_get_ap_config():
 # set AP config to which ESP station should connect
 # User should provide ssid, password, bssid
 def wifi_set_ap_config(ssid, pwd, bssid):
-    set_ap_config = slave_config_pb2.SlaveConfigPayload()
-    set_ap_config.msg = slave_config_pb2.SlaveConfigMsgType.TypeCmdSetAPConfig
+    set_ap_config = esp_hosted_config_pb2.EspHostedConfigPayload()
+    set_ap_config.msg = esp_hosted_config_pb2.EspHostedConfigMsgType.TypeCmdSetAPConfig
     set_ap_config.cmd_set_ap_config.ssid = str(ssid)
     set_ap_config.cmd_set_ap_config.pwd = str(pwd)
     set_ap_config.cmd_set_ap_config.bssid = str(bssid)
@@ -131,8 +131,8 @@ def wifi_set_ap_config(ssid, pwd, bssid):
 
 # Disconnect from AP
 def wifi_disconnect_ap():
-     disconnect_ap = slave_config_pb2.SlaveConfigPayload()
-     disconnect_ap.msg = slave_config_pb2.SlaveConfigMsgType.TypeCmdDisconnectAP
+     disconnect_ap = esp_hosted_config_pb2.EspHostedConfigPayload()
+     disconnect_ap.msg = esp_hosted_config_pb2.EspHostedConfigMsgType.TypeCmdDisconnectAP
      protodata = disconnect_ap.SerializeToString()
      #print("serialized data "+str(protodata))
      tp = transport.Transport_pserial(interface)
@@ -163,8 +163,8 @@ def wifi_disconnect_ap():
 #     2 : WIFI_BW_HT40 )
 
 def wifi_set_softap_config(ssid, pwd, chnl, ecn, max_conn, ssid_hidden, bw):
-    set_softap_config = slave_config_pb2.SlaveConfigPayload()
-    set_softap_config.msg = slave_config_pb2.SlaveConfigMsgType.TypeCmdSetSoftAPConfig
+    set_softap_config = esp_hosted_config_pb2.EspHostedConfigPayload()
+    set_softap_config.msg = esp_hosted_config_pb2.EspHostedConfigMsgType.TypeCmdSetSoftAPConfig
     set_softap_config.cmd_set_softap_config.ssid = str(ssid)
     set_softap_config.cmd_set_softap_config.pwd = str(pwd)
     set_softap_config.cmd_set_softap_config.chnl = chnl
@@ -203,8 +203,8 @@ def wifi_set_softap_config(ssid, pwd, chnl, ecn, max_conn, ssid_hidden, bw):
 #     2 : WIFI_BW_HT40 )
 
 def wifi_get_softap_config():
-    get_softap_config = slave_config_pb2.SlaveConfigPayload()
-    get_softap_config.msg = slave_config_pb2.SlaveConfigMsgType.TypeCmdGetSoftAPConfig
+    get_softap_config = esp_hosted_config_pb2.EspHostedConfigPayload()
+    get_softap_config.msg = esp_hosted_config_pb2.EspHostedConfigMsgType.TypeCmdGetSoftAPConfig
     protodata = get_softap_config.SerializeToString()
     #print("serialized data "+str(protodata))
     tp = transport.Transport_pserial(interface)
@@ -228,8 +228,8 @@ def wifi_get_softap_config():
 # output is list of Aplist class instances(ssid,chnl,rssi,bssid,ecn)
 
 def wifi_ap_scan_list():
-    get_ap_scan_list = slave_config_pb2.SlaveConfigPayload()
-    get_ap_scan_list.msg = slave_config_pb2.SlaveConfigMsgType.TypeCmdGetAPScanList
+    get_ap_scan_list = esp_hosted_config_pb2.EspHostedConfigPayload()
+    get_ap_scan_list.msg = esp_hosted_config_pb2.EspHostedConfigMsgType.TypeCmdGetAPScanList
     protodata = get_ap_scan_list.SerializeToString()
     #print("serialized data "+str(protodata))
     tp = transport.Transport_pserial(interface)
@@ -255,8 +255,8 @@ def wifi_ap_scan_list():
 # output is list of Stationlist class instances
 
 def wifi_connected_stations_list():
-    get_connected_stations_list = slave_config_pb2.SlaveConfigPayload()
-    get_connected_stations_list.msg = slave_config_pb2.SlaveConfigMsgType.TypeCmdGetConnectedSTAList
+    get_connected_stations_list = esp_hosted_config_pb2.EspHostedConfigPayload()
+    get_connected_stations_list.msg = esp_hosted_config_pb2.EspHostedConfigMsgType.TypeCmdGetConnectedSTAList
     protodata = get_connected_stations_list.SerializeToString()
     #print("serialized data "+str(protodata))
     tp = transport.Transport_pserial(interface)
