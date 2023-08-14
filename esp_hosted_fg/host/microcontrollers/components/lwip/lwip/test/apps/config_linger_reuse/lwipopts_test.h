f0bbd029abe6beb20c26fa374cea15213fd122e1
Yogesh Mantri <yogesh.mantri@espressif.com>
2023-08-14 12:56:52 +0800

    esp as mcu host, simplified code

--- esp_hosted_fg/host/microcontrollers/components/lwip/lwip/test/apps/config_linger_reuse/lwipopts_test.h
+++
@@ -1,7 +0,0 @@
-#ifndef LWIP_HDR_TEST_APPS_LWIPOPTS_TEST_H
-#define LWIP_HDR_TEST_APPS_LWIPOPTS_TEST_H
-
-#define LWIP_SO_LINGER             1
-#define SO_REUSE                   1
-
-#endif /* LWIP_HDR_TEST_APPS_LWIPOPTS_TEST_H */
