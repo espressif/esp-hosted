espressif/esp-hosted

commit 42fbb2e60205321e9f810209ad35cb7594494542

feat(esp_hosted_ng): Resolve cosmetic comments for PR clockrate paramter SDIO and SPI

esp_hosted_ng/host/include/utils.h
@@ -6,6 +6,10 @@
 
 #define pr_fmt(fmt) "%s:%s: " fmt, KBUILD_MODNAME, __func__
 
+#ifndef NUMBER_1M
+#define define NUMBER_1M 1000000
+#endif
+
 #ifndef MAC2STR
 #define MAC2STR(a) (a)[0], (a)[1], (a)[2], (a)[3], (a)[4], (a)[5]
 #define MACSTR "%02x:%02x:%02x:%02x:%02x:%02x"

esp_hosted_ng/host/sdio/esp_sdio.c
@@ -763,10 +763,10 @@ static int esp_probe(struct sdio_func *func,
 		return -ENOMEM;
 	}
 
-	if (sdio_context.sdio_clk_mhz){
+	if (sdio_context.sdio_clk_mhz) {
 		struct mmc_host *host = func->card->host;
-		u32 hz = sdio_context.sdio_clk_mhz * 1000000;
-		/* Expantion of mmc_set_clock that isnt exported */
+		u32 hz = sdio_context.sdio_clk_mhz * NUMBER_1M;
+		/* Expansion of mmc_set_clock that isnt exported */
 		if (hz < host->f_min)
 			hz = host->f_min;
 		if (hz > host->f_max)

esp_hosted_ng/host/spi/esp_spi.c
@@ -16,7 +16,6 @@
 #include "esp_stats.h"
 
 #define SPI_INITIAL_CLK_MHZ     10
-#define NUMBER_1M               1000000
 #define TX_MAX_PENDING_COUNT    100
 #define TX_RESUME_THRESHOLD     (TX_MAX_PENDING_COUNT/5)
 
@@ -686,7 +685,7 @@ int esp_init_interface_layer(struct esp_adapter *adapter, u32 speed)
 	adapter->if_ops = &if_ops;
 	adapter->if_type = ESP_IF_TYPE_SPI;
 	spi_context.adapter = adapter;
-	if(speed)
+	if (speed)
 		spi_context.spi_clk_mhz = speed;
 	else
 		spi_context.spi_clk_mhz = SPI_INITIAL_CLK_MHZ;

commit d94b482beed96d88a8ba817fa621d986b480496d

esp_hosted_ng/host/include/utils.h: fix double define left-over
Commit 42fbb2e60205321e9f810209ad35cb7594494542 introduced a wrong double
"define" on macro definition resulting in build failure, so let's remove
it.

Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>

esp_hosted_ng/host/include/utils.h
@@ -7,7 +7,7 @@
 #define pr_fmt(fmt) "%s:%s: " fmt, KBUILD_MODNAME, __func__
 
 #ifndef NUMBER_1M
-#define define NUMBER_1M 1000000
+#define NUMBER_1M 1000000
 #endif
 
 #ifndef MAC2STR